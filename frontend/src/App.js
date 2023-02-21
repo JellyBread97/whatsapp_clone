59d35c9f207c605a9cc95fcb5db0c67f9a321207
Nic <nic.dan.costea@gmail.com>
2023-02-21 09:21:14 +0100

    tokens to localStorage - Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import NavBar from "./components/navigation/NavBar";
+
+import LoginPage from "./components/pages/LoginPage";
+import HomePage from "./components/pages/HomePage";
+import { ToastContainer } from "react-toastify";
+
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      {/* <NavBar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <ToastContainer position="top-center" />
+    </Router>
+  );
 }
 
 export default App;
