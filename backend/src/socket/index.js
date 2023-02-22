f9d43a3e7247685c190b0906c05b6f4d9a7416cb
KLeonGreen <114649634+LeonMagnificat@users.noreply.github.com>
2023-02-22 16:07:34 +0100

    socket

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,7 +1,31 @@
 // import uniqid from "uniqid";
+import MessagesModel from "../api/message/model.js";
+import { JWTAuthMiddleware } from "../lib/auth/JWTauth.js";
 
-let users = [];
+let onlineUsers = [];
 
-export const newConnectionHandler = (client) => {
-  console.log("NEW CONNECTION - id: ", client.id);
+export const newConnectionHandler = (newClient) => {
+  console.log("NEW CONNECTION - id: ", newClient.id);
+
+  newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+  newClient.on("setUsername", (payload) => {
+    console.log(payload);
+    onlineUsers.push({ username: payload.username, socketId: newClient.id });
+    console.log("ONLINE USERS----", onlineUsers);
+
+    newClient.emit("loggedIn", onlineUsers);
+
+    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+  });
+
+  newClient.on("sendMessage", async (message) => {
+    console.log("NEW MESSAGE:", message);
+
+    // // const newMessage = new MessagesModel(message.message);
+    // const newMessage = new MessagesModel({ sender: "63f604c6153d28812f832dc2", content: { text: message.message.sender, media: "req.body.media" } });
+    // console.log("newMessage: ", newMessage);
+    // await newMessage.save();
+    // 3.1 Whenever we receive that new message we have to propagate that message to everybody but not sender
+    newClient.broadcast.emit("newMessage", message);
+  });
 };
